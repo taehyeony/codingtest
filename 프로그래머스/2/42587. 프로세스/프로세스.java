301b6b6ebb925a92b99ec5894f200701deae6f2e
taehyeonjeon <114964785+taehyeony@users.noreply.github.com>
2026-05-01 19:14:31 +0900

    [level 2] Title: 프로세스, Time: 1.67 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int p : priorities) {
+            pq.add(p);
+        }
+
+        int answer = 0;
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    pq.poll();
+                    answer++;
+                    if (i == location) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
